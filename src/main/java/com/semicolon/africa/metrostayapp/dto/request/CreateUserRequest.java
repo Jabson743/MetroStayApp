2612a4da8c7e11f16df78d01c8a0fada40535ea7
Jabson743 <138734239+Jabson743@users.noreply.github.com>
2025-01-22 18:45:20 -1200

    third commit

---
+++ src/main/java/com/semicolon/africa/metrostayapp/dto/request/CreateUserRequest.java
@@ -0,0 +1,4 @@
+package com.semicolon.africa.metrostayapp.dto.request;
+
+public class CreateUserRequest {
+}
---
+++ src/main/java/com/semicolon/africa/metrostayapp/dto/response/CreateUserResponse.java
@@ -0,0 +1,4 @@
+package com.semicolon.africa.metrostayapp.dto.response;
+
+public class CreateUserResponse {
+}
