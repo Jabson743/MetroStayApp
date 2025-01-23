f84263d42b1d55e453c8feb9a8c2826b290a1c75
Jabson743 <138734239+Jabson743@users.noreply.github.com>
2025-01-22 18:04:37 -1200

    first commit

---
+++ src/main/java/com/semicolon/africa/metrostayapp/MetroStayAppApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.metrostayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroStayAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetroStayAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semicolon/africa/metrostayapp/MetroStayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.metrostayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroStayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
